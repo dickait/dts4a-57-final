365a08b5cc98fc5a00fa331d5fa9c73c3eab19a4
Dicka Ariptian R <dickaariptian@gmail.com>
2022-10-26 23:24:31 +0700

    🚧 add api axios tmdb. give up for solo fighter. huft.

---
+++ src/components/ListMovies.jsx
@@ -0,0 +1,96 @@
+// di sini kita import apis/tmdb.js
+import tmdb from "../apis/tmdb";
+
+import React, { useEffect, useState } from "react";
+import { Box, Typography, CardContent, CardMedia, Rating } from "@mui/material";
+
+import Carousel from "react-material-ui-carousel";
+
+const ListMovies = () => {
+	const [movies, setMovies] = useState([]);
+	const baseUrlForMovie = "https://image.tmdb.org/t/p/w200";
+	useEffect(() => {
+		const fetchDataMovies = async () => {
+			try {
+				// Gunakan instance tmdb di sini
+				const responseDariTMDB = await tmdb.get(
+					// Nah di sini kita tidak perlu menuliskan terlalu panjang lagi
+					"/movie/popular"
+				);
+
+				// Jangan lupa set statenya
+				// Perhatikan di sini responseDariTMDB ada .data (response schema axios)
+				setMovies(responseDariTMDB.data.results);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+
+		fetchDataMovies();
+	}, []);
+
+	return (
+		// <Box className="boxy">
+		// 	<Typography variant="h5">
+		// 		Container ListMovies (Data Real)
+		// 	</Typography>
+
+		// 	{movies.map((movie) => {
+		// 		return <CardMovie movie={movie} />;
+		// 	})}
+		// </Box>
+		<Carousel
+			next={(active, next) => {
+				/* Do stuff */
+				console.log(`we left ${active}, and are now at ${next}`);
+			}}
+			prev={(active, prev) => {
+				/* Do other stuff */
+				console.log(`we left ${active}, and are now at ${prev}`);
+			}}
+			indicators={false}
+			navButtonsAlwaysVisible={true}
+		>
+			{movies.map((movie, i) => (
+				<div key={i}>
+					<CardContent
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							gap: 2,
+							width: 1,
+							minHeight: "500px",
+							color: "black",
+						}}
+					>
+						<Typography variant="h4">Popular</Typography>
+						<CardMedia
+							component="img"
+							sx={{ maxHeight: "400px", maxWidth: "200px" }}
+							// Kita gunakan image berdasarkan prefix dari image tmdb
+							image={`${baseUrlForMovie}${movie.poster_path}`}
+							// image={props.movie.poster_path}
+							alt={movie.title}
+						></CardMedia>
+						<Typography component="div" variant="body1">
+							{movie.title}
+						</Typography>
+						<Rating
+							value={movie.vote_average / 2}
+							precision={0.1}
+							readOnly
+						/>
+						<Typography variant="body2">
+							Release date: {movie.release_date}
+						</Typography>
+						<Typography variant="body2">
+							{movie.overview}
+						</Typography>
+					</CardContent>
+				</div>
+			))}
+		</Carousel>
+	);
+};
+
+export default ListMovies;
--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,34 +1,10 @@
 import React from "react";
-
-import { Box, Typography } from "@mui/material";
-
-import styles from "./HomePage.module.css";
-
-// Karena kita ingin menampilkan user, kita di sini cukup menggunakan useAuthState dan auth saja !
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../authentication/firebase";
+import ListMovies from "../components/ListMovies";
 
 const HomePage = () => {
-	// Di sini kita hanya perlu menggunakan user saja
-	const [user] = useAuthState(auth);
-
 	return (
 		<>
-			<Box className={styles.container}>
-				<Typography variant="body1">
-					Halo, ini adalah halaman utama
-				</Typography>
-
-				{/* Kita akan tampilkan email dari user di sini */}
-				{user ? (
-					<Typography variant="body1">
-						Email - <strong>{user.email}</strong>, uid -{" "}
-						<strong>{user.uid}</strong>
-					</Typography>
-				) : (
-					""
-				)}
-			</Box>
+			<ListMovies />
 		</>
 	);
 };
